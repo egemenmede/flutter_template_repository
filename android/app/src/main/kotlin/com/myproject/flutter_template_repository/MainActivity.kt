cc42aaebe8aaa9259516b68e1e8dd2f14178014e
Egemen MEDE <egemenmede@gmail.com>
2022-02-09 15:06:35 +0300

    add files

---
+++ android/app/src/main/kotlin/com/myproject/flutter_template_repository/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.myproject.flutter_template_repository
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/core/utils/contants.dart
@@ -0,0 +1,1 @@
+const String kMaterialAppTitle = 'Flutter Clean Architecture Title';
